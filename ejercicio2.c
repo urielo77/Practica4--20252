edee7cd6e3fd5d918290a6ad4d2c676edca4a557
urielo77 <uruiz7794@gmail.com>
2025-03-06 14:18:10 -0600

    Create ejercicio2.c

---
+++ ejercicio2.c
@@ -0,0 +1,22 @@
+//Uriel Ruiz
+//ejercicio1
+
+#include <stdio.h> 
+#include <stdlib.h>
+
+int main()
+{
+    int a=3;
+    int b=4;
+    
+    if(a>b)
+    {
+        printf("a es mayor \n");
+    }
+    else
+    {
+        printf("b es mayor o igual a a \n");
+    }
+    
+    return 0;
+}
