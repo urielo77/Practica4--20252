369fe40f6ea0bb832db135825a311811f6932e91
urielo77 <uruiz7794@gmail.com>
2025-03-06 14:04:26 -0600

    Create ejercicio1.c

---
+++ ejercicio1.c
@@ -0,0 +1,17 @@
+//Uriel Ruiz
+//ejercicio1
+
+#include <stdio.h> 
+#include <stdlib.h>
+
+int main()
+{
+    int a=3;
+    int b=2;
+    
+    if (a > b)
+    {
+        printf("a es mayor \n");
+    }
+    return 0;
+}
